d83686900bd1ddd1dc1920a4b995cb4d472b623a
Hackbright Student <info@hackbrightacademy.com>
2015-01-21 12:09:29 -0800

    Combined method call on line 10; added get method for dictionary and count creation

--- wordcount.py
+++ wordcount.py
@@ -1,36 +1,19 @@
 # from string import punctuation
-
 input_file = open('testfile.txt') # open/read twain.txt
 
-#creates dict called word_count and sets initial count to 1
+# creates dict called word_count and sets initial count to 1
 word_count = {}
 
 # creates list called separate_line of individual words in each line
 for line in input_file:
-        
-    separate_line = line.rstrip()
-    separate_line = separate_line.split(" ")
-
+    # remove white space and split line into list of words
+    separate_line = line.rstrip().split(" ")
     # iterates through each word in separate_line list
     for word in separate_line:
-        # turns words to camelcase
-        word = word.capitalize()
-#        word = word.translate(None, word.punctuation())
-        # if camelcase word is not a key in dict, add it to the dict
-        if word not in word_count.keys():
-            word_count[word] = 1 # adds as a key with value 1
-        # adds 1 to value for each time word
-        else:
-            word_count[word] = word_count[word] + 1 
+        # turns words to lowercase
+        word = word.lower()
+        # if lowercase word is not a key in dict, add as key; otherwise add 1 to count
+        word_count[word] = word_count.get(word, 0) + 1 
 
 for word, count in word_count.iteritems():
     print "%s %s" % (word, count)
-
-# string.split(",")
-# string.strip()
-# dict.get()
-# dict.iteritems()
-
-# sorts all alphabetically:
-# for word, count in sorted(word_count.iteritems()):
-#     print "%s %s" % (word, count)
