230a4e246278c709b66a2799a465782e0640d942
Hackbright Student <info@hackbrightacademy.com>
2015-01-20 18:45:59 -0800

    Eliminates duplicates based on capitalization

--- wordcount.py
+++ wordcount.py
@@ -1,15 +1,24 @@
-input_file = open('twain.txt') # open/read twain.txt
+input_file = open('testfile.txt') # open/read twain.txt
 
+#creates dict called word_count and sets initial count to 1
 word_count = {}
-# creates list of individual words in each line
+
+# creates list called separate_line of individual words in each line
 for line in input_file:
+        
     separate_line = line.rstrip()
     separate_line = separate_line.split(" ")
-    count = 1
-    for word in separate_line:
-        if word not in word_count.keys():
-            word_count[word] = count # adds as a key with value 1
-        else:
-            word_count[word] = (count + 1) # adds 1 to value for each time word appears as a key
 
-print word_count
+    # iterates through each word in separate_line list
+    for word in separate_line:
+        # turns words to camelcase
+        word = word.capitalize()
+        # if camelcase word is not a key in dict, add it to the dict
+        if word not in word_count.keys():
+            word_count[word] = 1 # adds as a key with value 1
+        # adds 1 to value for each time word
+        else:
+            word_count[word] = word_count[word] + 1 
+
+for word, count in word_count.iteritems():
+    print "%s %s" % (word, count)
